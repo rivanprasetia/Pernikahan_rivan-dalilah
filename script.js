d0df77e45f6092a9e5ad1119181bcf5831406caf
rivanprasetia <97424508+rivanprasetia@users.noreply.github.com>
2024-11-08 20:17:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ document.addEventListener('DOMContentLoaded', () => {
         if (name && name.trim() !== '') {
             sessionStorage.setItem('guestName', name);
          }
-         else {
-             alert('URL tidak valid. Harap akses halaman dengan format confirm.html?id=namaid.');
-             window.location.href = '/Pernikahan_rivan-dalilah/confirm.html'; // Ubah ke path sesuai kebutuhan
-         }
+//         else {
+//             alert('URL tidak valid. Harap akses halaman dengan format confirm.html?id=namaid.');
+//             window.location.href = '/Pernikahan_rivan-dalilah/confirm.html'; // Ubah ke path sesuai kebutuhan
+//         }
 
         const confirmButton = document.getElementById('confirm-button');
         if (confirmButton) {
